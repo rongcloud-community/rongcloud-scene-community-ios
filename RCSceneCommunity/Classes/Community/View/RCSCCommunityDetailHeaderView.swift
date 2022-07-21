4297e5c51162356424411dd537a0fd0e45c7735e
xuefeng <suixuefeng@rongcloud.cn>
2022-07-21 14:55:00 +0800

    fix：修复首页社区标题展示异常的问题

--- RCSceneCommunity/Classes/Community/View/RCSCCommunityDetailHeaderView.swift
+++ RCSceneCommunity/Classes/Community/View/RCSCCommunityDetailHeaderView.swift
@@ -158,6 +158,7 @@ class RCSCCommunityDetailHeaderView: UIView {
         groupNameLabel.snp.makeConstraints { make in
             make.top.equalTo(self).offset(7)
             make.leading.equalTo(self).offset(15)
+            make.trailing.equalTo(self).offset(-50)
         }
         
         addSubview(subNameLabel)
